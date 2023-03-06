3413632f07359f6fbc632b5e8baaa4469fa0b626
Tomasz Rogala <w69485@student.wsiz.edu.pl>
2023-03-06 14:15:08 +0100

    new models

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,7 +8,10 @@ namespace lab1
         [Fact]
         public void Test1()
         {
-
+            int i = 0;
+            i++;
+            Assert.True(true);
+            Console.WriteLine();
         }
     }
 }
